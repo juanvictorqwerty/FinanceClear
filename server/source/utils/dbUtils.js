f71766ed58f3d2133d7c0ee57ef7da56b508482b
MIKE Juan Victor <juanvictorqwerty@gmail.com>
2025-08-26 19:54:24 +0100

    remove the unique username

--- server/source/utils/dbUtils.js
+++ server/source/utils/dbUtils.js
@@ -4,7 +4,7 @@ import { pool } from '../config/database.js';
 const createUsersTableQuery = `
     CREATE TABLE IF NOT EXISTS \`user\` (
         email VARCHAR(255) PRIMARY KEY,
-        username VARCHAR(255) NOT NULL UNIQUE,
+        username VARCHAR(255) NOT NULL,
         password VARCHAR(255) NOT NULL,
         matricule VARCHAR(255) UNIQUE,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -22,8 +22,7 @@ const createProfilesTableQuery = `
         school_fee_due INT DEFAULT 365000 CHECK (school_fee_due >= 0),
         penalty_fee INT DEFAULT 0 CHECK (penalty_fee >= 0) NOT NULL,
         excess_fee INT DEFAULT 0 CHECK (excess_fee >= 0) NOT NULL,
-        FOREIGN KEY (email) REFERENCES \`user\`(email),
-        FOREIGN KEY (username) REFERENCES \`user\`(username)
+        FOREIGN KEY (email) REFERENCES \`user\`(email)
     )`;
 
 const createClearanceTableQuery = `
@@ -41,6 +40,14 @@ const createUsedUBA_receiptTableQuery = `
             receipt_used_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )`;
 
+const createAdminTableQuery = `
+    CREATE TABLE IF NOT EXISTS \`admin\` (
+        email VARCHAR(255) PRIMARY KEY,
+        username VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )`;
+
 
 async function createTableIfNotExists() {
     try {
@@ -48,6 +55,7 @@ async function createTableIfNotExists() {
         await pool.query(createProfilesTableQuery);
         await pool.query(createUsedUBA_receiptTableQuery);
         await pool.query(createClearanceTableQuery);
+        await pool.query(createAdminTableQuery);
         console.log('Database tables checked/created successfully.');
     } catch (error) {
         console.error('Error creating tables:', error);
